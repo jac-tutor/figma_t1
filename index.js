83880fda032c5b1497f12cfaa979559d04c20c6f
Jac Wang <lu7766lu7766@gmail.com>
2024-08-27 11:56:23 +0800

    js var, function

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 console.log("hello aaaa!")
+
+// 1
+// "9"
+// true
+// undefined
+// null
+// [1, 2, 3]
+// { a: 1, b: 2 }
+var a = 1
+var b // function
+let c = "c" // {}
+const d = "d"
+
+function funcA(param1, param2) {
+  if (true) {
+    var aa = "a"
+    let bb = "b"
+    const cc = "c"
+    bb = "dd"
+    bb = 1 + 2
+    bb = true
+    bb = aa // "a"
+    bb = function () {
+      console.log("hello bb")
+    } // bb()
+    // cc = "dd"
+    // console.log(cc)
+  }
+  return param1 + param2
+}
+
+const funAresult = funcA(1, 2)
+console.log(funAresult)
