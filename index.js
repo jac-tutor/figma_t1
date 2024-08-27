6d1d0228a3dc828ab80cbdbeefff792946692550
Jac Wang <lu7766lu7766@gmail.com>
2024-08-27 14:30:26 +0800

    if, logic使用

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ console.log("hello aaaa!")
 // { a: 1, b: 2 } // object // { a: 1, b: 2 }.a // { a: 1, b: 2 }["a"]
 
 // 宣告
-var a = 1
-var b // function
-let c = "c" // {}
-const d = "d" // PI = 3.14
+// var a = 1
+// var b // function
+// let c = "c" // {}
+// const d = "d" // PI = 3.14
 
 /**
  * A function that performs some operations and returns the sum of two parameters.
@@ -22,30 +22,30 @@ const d = "d" // PI = 3.14
  * @param {*} param2 - The second parameter to be added.
  * @return {*} The sum of param1 and param2.
  */
-function funcA(param1, param2) {
-  if (true) {
-    var aa = "a"
-    let bb = "b"
-    const cc = "c"
-    // cc = "dd" // error
-    // console.log(cc)
-    bb = "dd" // "dd"
-    bb = 1 + 2 // 3
-    bb = true // true
-    bb = aa // "a"
-    bb = function () {
-      console.log("hello bb")
-    } // bb() // hello bb
-  }
-  return param1 + param2
-}
-const funcB = function () {}
-// run function
-funcB()
+// function funcA(param1, param2) {
+//   if (true) {
+//     var aa = "a"
+//     let bb = "b"
+//     const cc = "c"
+//     // cc = "dd" // error
+//     // console.log(cc)
+//     bb = "dd" // "dd"
+//     bb = 1 + 2 // 3
+//     bb = true // true
+//     bb = aa // "a"
+//     bb = function () {
+//       console.log("hello bb")
+//     } // bb() // hello bb
+//   }
+//   return param1 + param2
+// }
+// // return 回傳
+// const funAresult = funcA(1, 2)
+// console.log(funAresult) // 3
 
-// return 回傳
-const funAresult = funcA(1, 2)
-console.log(funAresult) // 3
+// const funcB = function () {}
+// // run function
+// funcB()
 
 // if判斷式
 // else 可選 // else if 可選
@@ -56,9 +56,9 @@ console.log(funAresult) // 3
 // true || false // true
 // false || false // false
 // !false // true
-if (2 != 3 || 2 < 0) {
-  // ...
-}
+// if (2 != 3 || 2 < 0) {
+//   // ...
+// }
 // else if (false) {
 //   // ..
 // } else if (false) {
@@ -66,3 +66,26 @@ if (2 != 3 || 2 < 0) {
 // } else {
 //   // ..
 // }
+
+// let a = 10
+// if (a > 10) {
+//   console.log("a > 10")
+// } else if (a < 10) {
+//   console.log("a < 10")
+// } else {
+//   console.log("a == 10")
+// }
+
+// console.log(1 == "1") // true
+// console.log(1 === "1") // false
+// console.log(1 != "1") // false
+// console.log(1 !== "1") // true
+
+// console.log(true && true) // true
+// console.log(true && false) // false
+// console.log(true || true) // true
+// console.log(true || false) // true
+// console.log(false || false) // false
+
+// console.log(!false) // true
+// console.log(!true) // false
