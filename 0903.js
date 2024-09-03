4edf5af7f3fc293699d67e01482cdf2f6b1f5872
JacWang <lu7766lu7766@gmail.com>
2024-09-03 09:42:25 +0800

    p1, p2

--- 0903.js
+++ 0903.js
@@ -1,28 +1,33 @@
-let i = 0;
-if (i == 0) {
-}
-function funcA(a) {
-  a(9);
-  //   return a;
-}
-funcA(function (x) {
-  console.log("callback function", x);
-});
+// let i = 0;
+// if (i == 0) {
+// }
+// function funcA(a) {
+//   a(9);
+//   //   return a;
+// }
+// funcA(function (x) {
+//   console.log("callback function", x);
+// });
 
-for (let j = 0; j < 5; j++) {}
+// for (let j = 0; j < 5; j++) {}
 
-"hello world".substring(0, 5);
-[1, 2, 3].concat(4);
-[1, 2, 3][0];
-const obja = { a: "A" };
-obja.a;
+// "hello world".substring(0, 5);
+// [1, 2, 3].concat(4);
+// [1, 2, 3][0];
+// const obja = { a: "A" };
+// obja.a;
 
-[1, 2, 3].forEach(function (x, i, arr) {
-  console.log(x, i, arr);
-});
+// [1, 2, 3].forEach(function (x, i, arr) {
+//   console.log(x, i, arr);
+// });
 
-console.log(
-  [1, 2, 3].map(function (x) {
-    return x * 2;
-  })
-);
+// console.log(
+//   [1, 2, 3].map(function (x) {
+//     return x * 2;
+//   })
+// );
+
+// 練習1 "hello world"找出o的所有索引值
+// 練習2 "gave a nice day" 把所有文字第一個字變成大寫
+"a".toUpperCase();
+"hello world".split(" ");
