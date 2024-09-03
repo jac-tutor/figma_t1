44ac058dadc83683be132cbb34ec97c8a9d9f469
JacWang <lu7766lu7766@gmail.com>
2024-09-03 11:22:29 +0800

    回調函式，箭頭函式

--- 0903.js
+++ 0903.js
@@ -31,3 +31,22 @@
 // 練習2 "gave a nice day" 把所有文字第一個字變成大寫
 "a".toUpperCase();
 "hello world".split(" ");
+
+// function funcA(a) {
+//   return a;
+// }
+// const funcA = (a) => a;
+// const funcA = (a, b) => {
+//   return a + b;
+// };
+// const funcA = (a, b) => a + b;
+
+function forEach(arr, doFn) {
+  for (let i = 0; i < arr.length; i++) {
+    doFn(arr[i], i, arr);
+  }
+}
+
+forEach([1, 2, 3], function (x, i, arr) {
+  console.log(x, i, arr);
+});
